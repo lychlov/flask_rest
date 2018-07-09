3d4bfb6e81928c742897ae28eed1531d201fb6f3
lychlov <czk499658904@126.com>
2018-07-09 09:57:40 +0800

    Initial commit

---
+++ flask_rest.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+import os
+import urllib
+
+from flask import Flask, Response
+import requests
+from lxml import etree
+
+app = Flask(__name__)
+IMG_STORE = "/home/ops/work_space/IMG_STORE/"
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/channel/<channel_name>')
+def show_channel_image(channel_name):
+    file_name_png = IMG_STORE + "/channel/"+channel_name+".png"
+    file_name_jpg = IMG_STORE + "/channel/"+channel_name+".jpg"
+    file_unknown_png = IMG_STORE + "/channel/"+"TV.png"
+
+    if os.path.exists(file_name_jpg):
+        image = file(file_name_jpg)
+        return Response(image, mimetype="image/jpeg")
+    if os.path.exists(file_name_png):
+        image = file(file_name_png)
+        return Response(image, mimetype="image/jpeg")
+    image = file(file_unknown_png)
+    return Response(image, mimetype="image/jpeg")
+
+
+
+@app.route('/tv/<tvname>')
+def show_tv_image(tvname):
+    # show the tv image for that TV
+    target = 'https://www.douban.com/search?cat=1002&q=' + tvname
+    print (target)
+    html = requests.get(target).content.decode('utf-8')
+    doc_tree = etree.HTML(html)
+    image_links = doc_tree.xpath('//*[@id="content"]/div/div[1]/div[3]/div[2]/div[1]/div[1]/a/img/@src')
+    filename = image_links[0].split('/')[-1]
+    path_to_img = IMG_STORE + filename
+    if not os.path.exists(path_to_img):
+        print "New download"
+        save_img(image_links[0], filename)
+    image = file(IMG_STORE + filename)
+    resp = Response(image, mimetype="image/jpeg")
+    return resp
+
+
+def save_img(url, filename):
+    # filename = url[:]
+    try:
+        urllib.urlretrieve(url, filename=IMG_STORE + filename)
+    except IOError as e:
+        print '文件操作失败', e
+    except Exception as e:
+        print '错误 ：', e
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=False)
