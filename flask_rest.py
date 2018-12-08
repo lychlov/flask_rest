a50179e4695f4560a256309f3d40a9df674e3acc
lychlov <czk499658904@126.com>
2018-12-08 16:10:10 +0800

    debug

--- flask_rest.py
+++ flask_rest.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
+import json
 import os
-import urllib
-
+from urllib import request
+from aip import AipOcr
 from flask import Flask, Response
+from bs4 import BeautifulSoup
 import requests
-from lxml import etree
 
 app = Flask(__name__)
-IMG_STORE = "/home/ops/work_space/IMG_STORE/"
+IMG_STORE = "/Users/zhikuncheng/Documents/IMG_STORE/"
+""" 你的 APPID AK SK """
+APP_ID = '11293420'
+API_KEY = 'kRdCPbCKskPgtNy7upj420lp'
+SECRET_KEY = 'yepRFx7FyNCiQUp18wDawepNU6Dclf0g'
 
 
 @app.route('/')
@@ -22,13 +27,34 @@ def show_channel_image(channel_name):
     file_unknown_png = IMG_STORE + "/channel/" + "TV.png"
 
     if os.path.exists(file_name_jpg):
-        image = file(file_name_jpg)
+        image = open(file_name_jpg, 'r')
         return Response(image, mimetype="image/jpeg")
     if os.path.exists(file_name_png):
-        image = file(file_name_png)
+        image = open(file_name_png, 'r')
         return Response(image, mimetype="image/jpeg")
-    image = file(file_unknown_png)
+    image = open(file_unknown_png, 'r')
     return Response(image, mimetype="image/jpeg")
+
+
+# OCR识别
+@app.route('/image/<image_file>')
+def orc_image(image_file):
+    """ 你的 APPID AK SK """
+    APP_ID = '11293420'
+    API_KEY = 'kRdCPbCKskPgtNy7upj420lp'
+    SECRET_KEY = 'yepRFx7FyNCiQUp18wDawepNU6Dclf0g'
+
+    client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
+    # options = {}
+    # options["language_type"] = "ENG"
+    try:
+        image = get_file_content(IMG_STORE + image_file)
+    except Exception as e:
+        return Response(e)
+    """ 调用通用文字识别, 图片参数为本地图片 """
+    result = client.basicGeneral(image)
+
+    return Response(json.dumps(result, ensure_ascii=False))
 
 
 @app.route('/tv/<tvname>')
@@ -37,29 +63,35 @@ def show_tv_image(tvname):
     # 访问豆瓣根据剧名进行搜索
     target = 'https://www.douban.com/search?cat=1002&q=' + tvname
     html = requests.get(target).content.decode('utf-8')
-    doc_tree = etree.HTML(html)
+    soup = BeautifulSoup(html, 'lxml')
     # 截取封面图片链接
-    image_links = doc_tree.xpath('//*[@id="content"]/div/div[1]/div[3]/div[2]/div[1]/div[1]/a/img/@src')
-    filename = image_links[0].split('/')[-1]
+    image_link = soup.find('div', class_='search-result').find('div', class_='result-list').find('div',
+                                                                                                 class_='result').find(
+        'img').get('src')
+    filename = image_link.split('/')[-1]
     path_to_img = IMG_STORE + filename
     # 如果曾经下载过图片就从本地获取，否则下载图片并保存
     if not os.path.exists(path_to_img):
-        print "New download"
-        save_img(image_links[0], filename)
-    image = file(IMG_STORE + filename)
+        print("New download")
+        save_img(image_link, filename)
+    image = open(IMG_STORE + filename, 'rb').read()
     # 返回图片结果
     resp = Response(image, mimetype="image/jpeg")
     return resp
 
 
+def get_file_content(filePath):
+    with open(filePath, 'rb') as fp:
+        return fp.read()
+
+
 def save_img(url, filename):
-    # filename = url[:]
     try:
-        urllib.urlretrieve(url, filename=IMG_STORE + filename)
+        request.urlretrieve(url, filename=IMG_STORE + filename)
     except IOError as e:
-        print '文件操作失败', e
+        print('文件操作失败', e)
     except Exception as e:
-        print '错误 ：', e
+        print('错误 ：', e)
 
 
 if __name__ == '__main__':
